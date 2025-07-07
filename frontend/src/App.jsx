2026c81c616a840360612baf7ab6a2a55fd4a12e
Raghav <raghavk2806@gmail.com>
2025-07-07 15:33:19 +0530

    front auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import ErrorPage from "./pages/ErrorPage";
-import Example from "./pages/SignUp";
+import AuthenticationPage, {action as authAction} from './pages/Authentication';
+import Dashboard from "./pages/Dashboard";
+import { checkAuthToken, loaderToken } from './util/auth';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    loader: loaderToken,
+    id: 'root',
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <Example />}
+      { index: true, 
+        element: <AuthenticationPage />,
+        action: authAction,
+      },
+      { index: '/dashboard',
+        element: <Dashboard />
+      }
     ],
   },
 ]);
--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -1,4 +1,9 @@
-export default function Example() {
+import {Form, Link, useSearchParams, useActionData} from 'react-router-dom';
+function AuthForm() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const isLogin = searchParams.get('mode') === 'login'
+
+  const data = useActionData();
   return (
     <>
       {/*
@@ -17,13 +22,13 @@ export default function Example() {
             className="mx-auto h-10 w-auto"
           />
           <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Sign in to your account
+            {isLogin ? 'Log into your Account' : 'Sign into your account'}
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
           <div className="bg-white px-6 py-12 shadow-sm sm:rounded-lg sm:px-12">
-            <form action="#" method="POST" className="space-y-6">
+            <Form action="#" method="POST" className="space-y-6">
               <div>
                 <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
                   Email address
@@ -56,59 +61,28 @@ export default function Example() {
                 </div>
               </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex gap-3">
-                  <div className="flex h-6 shrink-0 items-center">
-                    <div className="group grid size-4 grid-cols-1">
-                      <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
-                      />
-                      <svg
-                        fill="none"
-                        viewBox="0 0 14 14"
-                        className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
-                      >
-                        <path
-                          d="M3 8L6 11L11 3.5"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          className="opacity-0 group-has-checked:opacity-100"
-                        />
-                        <path
-                          d="M3 7H11"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          className="opacity-0 group-has-indeterminate:opacity-100"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                  <label htmlFor="remember-me" className="block text-sm/6 text-gray-900">
-                    Remember me
-                  </label>
-                </div>
-
+              <div className="flex items-center justify-end">
                 <div className="text-sm/6">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  <Link to="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
                     Forgot password?
-                  </a>
+                  </Link>
                 </div>
               </div>
 
               <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                <Link 
+                    to={`?mode=${isLogin ? 'signup' : 'login'}`} 
+                    className="flex w-full justify-center mb-2 rounded-md bg-white px-2 py-1.5 text-sm/6 font-semibold text-indigo-600 border-indigo-600 border-2 shadow-xs hover:bg-indigo-500 hover:text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                    {isLogin ? 'Sign Up' : 'Login'}
+                </Link>
+                <button 
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                    Save
                 </button>
               </div>
-            </form>
+            </Form>
 
             <div>
               <div className="relative mt-10">
@@ -154,3 +128,5 @@ export default function Example() {
     </>
   )
 }
+
+export default AuthForm;

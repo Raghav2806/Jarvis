cceaabd3e109bc482bfa578d061bb612eee04511
Raghav <raghavk2806@gmail.com>
2025-07-14 17:20:56 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    fs: {
+      strict: false
+    }
+  },
+  build: {
+    outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  base: './',
 })
